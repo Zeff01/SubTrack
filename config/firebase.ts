a53b38565d6ac60cb9954dddf9811b60655faede
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-06-11 15:23:05 +0800

    updated

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,8 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCfDPY0j4Zgdpb51jK9fyoHEXFa7qXFaRk",
@@ -13,6 +12,7 @@ const firebaseConfig = {
   appId: "1:586277645670:web:cd9c0b2a09e5bb402beca7",
   measurementId: "G-2MCL89NB70"
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
